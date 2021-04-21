0307dcd1c4401d4bb23870ec7dd920e6c518047b
Jason Agustino <jagu0001@student.monash.edu>
2021-04-21 23:32:02 +0800

    Initial Commit

---
+++ FIT3178-Assignment-Bill-Tracking-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIT3178-Assignment-Bill-Tracking-App
+//
+//  Created by user190204 on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
